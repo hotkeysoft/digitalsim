5a98c8b419336a130765c43e7339126b23d97227
hotkeysoft <dev@hotkeysoft.net>
2018-04-26 18:18:42 +0000

    Added test for complex component

--- proto/TestGates.cpp
+++ proto/TestGates.cpp
@@ -160,8 +160,7 @@ namespace UnitTests
 			delete gate;
 		}
 
-
-		TEST_METHOD(TestCustomDecoder)
+		GateBase* BuildDecoder()
 		{
 			GateBase * component = new GateBase("DECODER");
 			component->AddInput("EN");
@@ -203,6 +202,14 @@ namespace UnitTests
 			component->GetGate("NOTI1")->GetPin("out")->ConnectTo(component->GetGate("AND0")->GetPin("in2"));
 			component->GetGate("NOTI1")->GetPin("out")->ConnectTo(component->GetGate("AND1")->GetPin("in2"));
 
+			return component;
+		}
+
+		TEST_METHOD(TestCustomDecoder)
+		{
+			GateBase * component = BuildDecoder();
+			Assert::IsNotNull(component);
+
 			LogicTools::IOStateList out = LogicTools::GetTruthTable(component);
 			LogicTools::IOStateList compare = {
 				IOPin::LOW, IOPin::LOW, IOPin::LOW, IOPin::LOW,
@@ -218,6 +225,66 @@ namespace UnitTests
 			delete component;
 		}
 
+		TEST_METHOD(TestCustomComplexComponent)
+		{
+			GateBase * decoder1 = BuildDecoder();
+			Assert::IsNotNull(decoder1);
+
+			GateBase * decoder2 = BuildDecoder();
+			Assert::IsNotNull(decoder2);
+
+			NOTGate *notI1 = new NOTGate();
+
+			GateBase * decoder3to8 = new GateBase("Decoder3to8");
+			decoder3to8->AddInput("I0");
+			decoder3to8->AddInput("I1");
+			decoder3to8->AddInput("I2");
+			decoder3to8->AddOutput("Y0");
+			decoder3to8->AddOutput("Y1");
+			decoder3to8->AddOutput("Y2");
+			decoder3to8->AddOutput("Y3");
+			decoder3to8->AddOutput("Y4");
+			decoder3to8->AddOutput("Y5");
+			decoder3to8->AddOutput("Y6");
+			decoder3to8->AddOutput("Y7");
+
+			decoder3to8->AddGate("dec1", decoder1);
+			decoder3to8->AddGate("dec2", decoder2);
+			decoder3to8->AddGate("notI1", notI1);
+
+			decoder1->GetPin("Y0")->ConnectTo(decoder3to8->GetPin("Y0"));
+			decoder1->GetPin("Y1")->ConnectTo(decoder3to8->GetPin("Y1"));
+			decoder1->GetPin("Y2")->ConnectTo(decoder3to8->GetPin("Y2"));
+			decoder1->GetPin("Y3")->ConnectTo(decoder3to8->GetPin("Y3"));
+			decoder2->GetPin("Y0")->ConnectTo(decoder3to8->GetPin("Y4"));
+			decoder2->GetPin("Y1")->ConnectTo(decoder3to8->GetPin("Y5"));
+			decoder2->GetPin("Y2")->ConnectTo(decoder3to8->GetPin("Y6"));
+			decoder2->GetPin("Y3")->ConnectTo(decoder3to8->GetPin("Y7"));
+
+			decoder3to8->GetPin("I0")->ConnectTo(decoder1->GetPin("I0"));
+			decoder3to8->GetPin("I0")->ConnectTo(decoder2->GetPin("I0"));
+			decoder3to8->GetPin("I1")->ConnectTo(decoder1->GetPin("I1"));
+			decoder3to8->GetPin("I1")->ConnectTo(decoder2->GetPin("I1"));
+			decoder3to8->GetPin("I2")->ConnectTo(decoder2->GetPin("EN"));
+			decoder3to8->GetPin("I2")->ConnectTo(notI1->GetPin("in"));
+			notI1->GetPin("out")->ConnectTo(decoder1->GetPin("EN"));
+
+			LogicTools::IOStateList out = LogicTools::GetTruthTable(decoder3to8);
+
+			LogicTools::IOStateList compare = {
+				IOPin::HI, IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::LOW,
+				IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::HI ,IOPin::LOW,IOPin::LOW,IOPin::LOW,
+				IOPin::LOW,IOPin::LOW,IOPin::HI ,IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::LOW,
+				IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::HI ,IOPin::LOW,
+				IOPin::LOW,IOPin::HI ,IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::LOW,
+				IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::HI ,IOPin::LOW,IOPin::LOW,
+				IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::HI ,IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::LOW,
+				IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::LOW,IOPin::HI 
+			};
+
+			Assert::AreEqual(compare, out);
+			delete decoder3to8;
+		}
 
 	};
 }
